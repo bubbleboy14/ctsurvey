878d6055dded42fe446d5d4eecc29164a4534da3
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-21 20:54:39 -0700

    basic core functions: editor(), page(), pages(), edit(); _{} w/ nodes

--- ctsurvey/js/survey/core.js
+++ ctsurvey/js/survey/core.js
@@ -1,3 +1,40 @@
 survey.core = {
-	
+	_: {
+		pages: CT.dom.div(null, "pages"),
+		images: CT.dom.div(null, "images"),
+		questions: CT.dom.div(null, "questions"),
+	},
+	editor: function(data) {
+		return [
+			CT.dom.div(data.map(function(d) {
+				return CT.dom.link(d.title, function() {
+					survey.core.edit(d);
+				});
+			}), "surveys"),
+			[
+				survey.core._.pages,
+				CT.dom.div([
+					survey.core._.images,
+					survey.core._.questions
+				], "page")
+			]
+		];
+	},
+	page: function(page) {
+		CT.dom.setContent(survey.core._.questions, page.questions);
+		CT.dom.setContent(survey.core._.images, page.images.map(CT.dom.img));
+	},
+	pages: function(pages) {
+		CT.dom.setContent(survey.core._.pages, pages.map(function(p, i) {
+			return CT.dom.link(i + 1, function() {
+				survey.core.page(p);
+			}));
+		]);
+		survey.core.page(pages[0]);
+	},
+	edit: function(item) { // TODO: cache this....
+		CT.db.get("page", survey.core.pages, null, null, null, {
+			survey: item.key
+		});
+	}
 };
