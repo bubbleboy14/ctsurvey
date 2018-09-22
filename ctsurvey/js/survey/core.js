f24311ddb21bfae2463f9c72af8b22a46ee26a25
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-21 22:29:48 -0700

    core: machinery around new items in page() and editor(); centralized field generation

--- ctsurvey/js/survey/core.js
+++ ctsurvey/js/survey/core.js
@@ -8,6 +8,10 @@ survey.core = {
 			page: {
 				images: [],
 				questions: []
+			},
+			question: {
+				classname: "w1",
+				blurs: ["what's the question?", "question please", "type the question"]
 			}
 		},
 		info: CT.dom.div(null, "info"),
@@ -18,37 +22,48 @@ survey.core = {
 	editor: function(data) {
 		var _ = survey.core._;
 		return [
-			CT.dom.link("new survey", function() {
-				survey.core.edit(CT.merge({
-					user: user.core.get("key")
-				}, _.blank.survey));
-			}),
-			CT.dom.div(data.map(function(d) {
-				return CT.dom.link(d.title, function() {
-					survey.core.edit(d);
-				});
-			}), "surveys"),
-			[
+			CT.dom.div([
+				CT.dom.link("new survey", function() {
+					survey.core.edit(CT.merge({
+						user: user.core.get("key")
+					}, _.blanks.survey));
+				}),
+				data.map(function(d) {
+					return CT.dom.link(d.title, function() {
+						survey.core.edit(d);
+					});
+				})
+			], "surveys"),
+			CT.dom.div([
 				survey.core._.pages,
 				CT.dom.div([
 					_.info,
 					_.images,
 					_.questions
 				], "page")
-			]
+			], "main")
 		];
 	},
+	//"smartField": function(cb, classname, id, value, type, blurs, isTA) {
 	page: function(page) {
-		var qz = page.questons.map(function(q) {
-			CT.dom.textArea(null, q)
-		}), newq = CT.dom.inputEnterCallback(CT.dom.textArea(), function(val) {
-			qz.appendChild(CT.dom.textArea(null, val));
-		}), surv = CT.data.get(page.survey),
-			title = CT.dom.field(null, surv.title),
-			blurb = CT.dom.textArea(null, surv.blurb);
-		CT.dom.setContent(survey.core._.info, [title, blurb]);
-		CT.dom.setContent(survey.core._.questions, [qz, newq]);
-		CT.dom.setContent(survey.core._.images, page.images.map(CT.dom.img));
+		var _ = survey.core._, qfield = function(val, opts) {
+			return CT.dom.smartField(CT.merge({
+				value: val
+			}, opts, _.blanks.question));
+		}, qz = page.questions.map(qfield), newq = qfield("", {
+			isTa: true,
+			cb: function(val) {
+				qz.appendChild(qfield(val, { isTA: true }));
+			}
+		}), title = qfield(_.cursur.title, {
+				blurs: ["what's the title?", "what do you call this survey?", "survey title?"]
+			}),
+			blurb = qfield(_.cursur.blurb, {
+				blurs: ["what's the blurb?", "describe", "tell me more", "gimme some info"]
+			});
+		CT.dom.setContent(_.info, [title, blurb]);
+		CT.dom.setContent(_.questions, [qz, newq]);
+		CT.dom.setContent(_.images, page.images.map(CT.dom.img));
 	},
 	pages: function(pages) {
 		var _ = survey.core._;
@@ -66,7 +81,9 @@ survey.core = {
 	},
 	edit: function(item) { // TODO: cache this....
 		survey.core._.cursur = item;
-		CT.db.get("page", survey.core.pages, null, null, null, {
+		if (!item.key) // new survey
+			survey.core.pages([]);
+		else CT.db.get("page", survey.core.pages, null, null, null, {
 			survey: item.key
 		});
 	}
