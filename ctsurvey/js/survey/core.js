5833cf2e68c1f45891d7ba4df7aa7f6fb46eb627
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-21 21:38:39 -0700

    core: page() makes textareas for questions, including new question; pages() fix

--- ctsurvey/js/survey/core.js
+++ ctsurvey/js/survey/core.js
@@ -2,7 +2,7 @@ survey.core = {
 	_: {
 		pages: CT.dom.div(null, "pages"),
 		images: CT.dom.div(null, "images"),
-		questions: CT.dom.div(null, "questions"),
+		questions: CT.dom.div(null, "questions")
 	},
 	editor: function(data) {
 		return [
@@ -21,15 +21,20 @@ survey.core = {
 		];
 	},
 	page: function(page) {
-		CT.dom.setContent(survey.core._.questions, page.questions);
+		var qz = page.questons.map(function(q) {
+			CT.dom.textArea(null, q)
+		}), newq = CT.dom.inputEnterCallback(CT.dom.textArea(), function(val) {
+			qz.appendChild(CT.dom.textArea(null, val));
+		});
+		CT.dom.setContent(survey.core._.questions, [qz, newq]);
 		CT.dom.setContent(survey.core._.images, page.images.map(CT.dom.img));
 	},
 	pages: function(pages) {
 		CT.dom.setContent(survey.core._.pages, pages.map(function(p, i) {
 			return CT.dom.link(i + 1, function() {
 				survey.core.page(p);
-			}));
-		]);
+			});
+		}));
 		survey.core.page(pages[0]);
 	},
 	edit: function(item) { // TODO: cache this....
