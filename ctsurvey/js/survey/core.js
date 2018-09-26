5359169ade52227fb46a000f87e60be434e3e2df
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-25 22:52:46 -0700

    rmed register() from core{} (now in viewer{})

--- ctsurvey/js/survey/core.js
+++ ctsurvey/js/survey/core.js
@@ -24,44 +24,6 @@ survey.core = {
 		questions: CT.dom.div(null, "questions"),
 		newpage: CT.dom.button("new page", null, "right hidden")
 	},
-	register: function(cb) {
-		var qf = survey.core.qfield, uname = qf(null, {
-			id: "uname",
-			blurs: ["what's your name?", "what should we call you?", "name?", "who are you?"]
-		}), umail = qf(null, {
-			id: "umail",
-			blurs: ["what's your email?", "email address?", "electronic mailing address?"]
-		}), uzip = qf(null, {
-			id: "uzip",
-			blurs: ["what's your zipcode?", "zipcode please", "zipcode?", "what about your zipcode?"]
-		});
-		(new CT.modal.Modal({
-			transition: "slide",
-			noClose: true,
-			content: [
-				"tell us about yourself! name and email are optional. zipcode is required.",
-				uname, umail, uzip, CT.dom.button("continue", function() {
-					var n = CT.dom.getFieldValue("uname"),
-						m = CT.dom.getFieldValue("umail"),
-						z = CT.parse.stripToZip(CT.dom.getFieldValue("uzip"));
-					if (!z)
-						return alert("please enter a valid zipcode");
-					CT.net.post({
-						path: "/_survey",
-						params: {
-							action: "register",
-							zip: z,
-							name: n,
-							email: m
-						}, cb: function(key) {
-							survey.core._.person = key;
-							cb();
-						}
-					});
-				})
-			]
-		})).show();
-	},
 	save: function(obj, cb) {
 		CT.net.post({
 			path: "/_db",
