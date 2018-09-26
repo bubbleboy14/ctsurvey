5889885bc59bc5c085ea203602ffabbd2cb940cb
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-25 22:07:43 -0700

    /_survey handler: register action

--- ctsurvey/_survey.py
+++ ctsurvey/_survey.py
@@ -1,12 +1,16 @@
 from cantools.web import log, respond, succeed, cgi_get
 from cantools.db import edit
-from model import db, Survey
+from model import db, getzip, Survey, Person
 
 def response():
-    action = cgi_get("action", choices=["create", "respond"])
-    if action == "create":
-        pass
-    elif action == "respond":
-        pass
+    action = cgi_get("action", choices=["register"])
+    if action == "register":
+        z = getzip(cgi_get("zip"))
+        p = Person()
+        p.zipcode = z.key
+        p.name = cgi_get("name", required=False)
+        p.email = cgi_get("email", required=False)
+        p.put()
+        succeed(p.key)
 
 respond(response)
