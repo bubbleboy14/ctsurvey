0fbb25a604fcc75369a08eff5fb92944d448576c
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-25 22:37:29 -0700

    main loader: if no hash, redir to /; get survey from hash, register user

--- ctsurvey/js/survey/pages/main.js
+++ ctsurvey/js/survey/pages/main.js
@@ -3,12 +3,18 @@ CT.require("core");
 CT.require("user.core");
 CT.require("survey.core");
 
+var hash = location.hash.slice(1);
+if (!hash)
+	location = "/"; // whatever, kick em back
+
 CT.onload(function() {
 	CT.initCore();
-	survey.core.register(function() {
-		// load up the survey
-		// - questionnaire (->Profile)
-		// - pages (w/o questions, timed)
-		// - pages (w/ questions)
+	CT.db.one(hash, function(survey) {
+		survey.core.register(function() {
+			// load up the survey
+			// - questionnaire (->Profile)
+			// - pages (w/o questions, timed)
+			// - pages (w/ questions)
+		});
 	});
 });
