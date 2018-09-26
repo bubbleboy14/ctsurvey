0ad54d7b3e875c049dfba5c971cf8b132ecd9c60
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-26 01:12:41 -0700

    config: apikey, blurs{}, profile_prompt""

--- ctsurvey/js/config.js
+++ ctsurvey/js/config.js
@@ -1,5 +1,13 @@
 {
-	"surveys": {
-		
-	}
+	"apikey": null,
+	"blurs": {
+		"title": ["what's the title?", "what do you call this survey?", "survey title?"],
+		"blurb": ["what's the blurb?", "describe", "tell me more", "gimme some info"],
+		"question": ["what's the question?", "question please", "type the question"],
+		"answer": ["what do you think?", "please respond thoughtfully", "thoughts?"],
+		"name": ["what's your name?", "what should we call you?", "name?", "who are you?"],
+		"email": ["what's your email?", "email address?", "electronic mailing address?"],
+		"zip": ["what's your zipcode?", "zipcode please", "zipcode?", "what about your zipcode?"]
+	},
+	"profile_prompt": "tell us about yourself! name and email are optional. zipcode is required."
 }
