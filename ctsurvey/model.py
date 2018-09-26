57e2d6b0f9f3ecc93b5d54989a900168b46b9d7a
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-25 20:39:25 -0700

    model: ZipCode and getzip(); Person; Profile; Answer.person (no name or email [now on Person])

--- ctsurvey/model.py
+++ ctsurvey/model.py
@@ -20,9 +20,44 @@ class Page(db.TimeStampedBase):
     images = db.ForeignKey(kind=Image, repeated=True)
     questions = db.String(repeated=True)
 
-class Answer(db.TimeStampedBase):
-    page = db.ForeignKey(kind=Page)
-    question = db.Integer()
+class ZipCode(db.TimeStampedBase):
+    code = db.String()
+    city = db.String()
+    state = db.String()
+    county = db.String()
+    label = "code"
+
+def getzip(code):
+    if len(code) < 5:
+        from cantools.net import fail
+        fail("invalid zip code")
+    try:
+        code = str(int(code.strip()[:5]))
+        while len(code) < 5: # preceding 0's
+            code = '0'+code
+    except:
+        from cantools.net import fail
+        fail("invalid zip code")
+    zipcode = ZipCode.query().filter(ZipCode.code == code).get()
+    if not zipcode:
+        from cantools.web import fetch
+        city, state, county = fetch(ZIPDOMAIN, path="/%s"%(code,), asjson=True)
+        zipcode = ZipCode(code=code, city=city, state=state, county=county)
+        zipcode.put()
+    return zipcode
+
+class Person(db.TimeStampedBase):
+    zipcode = db.ForeignKey(kind=ZipCode)
     name = db.String()  # optional
     email = db.String() # optional
+
+class Profile(db.TimeStampedBase):
+    survey = db.ForeignKey(kind=Survey)
+    person = db.ForeignKey(kind=Person)
+    demographics = db.String(repeated=True) # 1 per survey demo
+
+class Answer(db.TimeStampedBase):
+    person = db.ForeignKey(kind=Person)
+    page = db.ForeignKey(kind=Page)
+    question = db.Integer()
     response = db.Text()
