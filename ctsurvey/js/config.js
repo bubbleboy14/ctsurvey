5822a4beb5c73ffcc909be5ba961065c4e59985b
bubbleboy14 <mario.balibrera@gmail.com>
2018-09-27 03:12:58 -0700

    timeout lowered to 5 (cfg)

--- ctsurvey/js/config.js
+++ ctsurvey/js/config.js
@@ -1,6 +1,6 @@
 {
 	"apikey": null,
-	"timeout": 15,
+	"timeout": 5,
 	"blurs": {
 		"title": ["what's the title?", "what do you call this survey?", "survey title?"],
 		"blurb": ["what's the blurb?", "describe", "tell me more", "gimme some info"],
