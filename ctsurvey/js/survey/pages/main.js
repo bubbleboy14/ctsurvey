08daea9abf071ca02aa67d2bf4cace8250df11e5
bubbleboy14 <mario.balibrera@gmail.com>
2018-10-09 00:16:18 -0700

    main (viewer) page loader: use survey.core.hash()

--- ctsurvey/js/survey/pages/main.js
+++ ctsurvey/js/survey/pages/main.js
@@ -4,12 +4,8 @@ CT.require("user.core");
 CT.require("survey.core");
 CT.require("survey.viewer");
 
-var hash = location.hash.slice(1) || core.config.ctsurvey.default_survey;
-if (!hash)
-	location = "/"; // whatever, kick em back
-
 CT.onload(function() {
 	CT.initCore();
 	CT.dom.remove("ctll");
-	survey.viewer.init(hash);
+	survey.viewer.init(survey.core.hash());
 });
