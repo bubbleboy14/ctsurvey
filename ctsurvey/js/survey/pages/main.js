ff54c9d47b9436ebdeb691b90ba35ceac9fbf7f9
bubbleboy14 <mario.balibrera@gmail.com>
2018-10-08 23:40:02 -0700

    main (viewer) page loader: if no hash, fall back to cfg.default_survey

--- ctsurvey/js/survey/pages/main.js
+++ ctsurvey/js/survey/pages/main.js
@@ -4,7 +4,7 @@ CT.require("user.core");
 CT.require("survey.core");
 CT.require("survey.viewer");
 
-var hash = location.hash.slice(1);
+var hash = location.hash.slice(1) || core.config.ctsurvey.default_survey;
 if (!hash)
 	location = "/"; // whatever, kick em back
 
